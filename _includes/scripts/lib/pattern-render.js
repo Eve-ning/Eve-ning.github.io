b6d97d645e4cf7995e0b2345b9ee9c9713714aac
Eve-ning <johnchangqi@hotmail.com>
2024-07-08 13:23:16 +0800

    Update post

--- _includes/scripts/lib/pattern-render.js
+++ _includes/scripts/lib/pattern-render.js
@@ -1,84 +1,116 @@
 (function () {
-  let width = 200,
-    height = 300,
+  let width = 100,
+    height = 200,
     noteWidthScale = 0.92,
     noteHeightScale = 0.05;
 
   // Select the <pre><code> element containing the data
-  const codeElement = document.querySelector('pre code');
-  const canvas = document.createElement('canvas');
+  var codeElements = document.querySelectorAll(
+    'pre code[class^="language-osumania"], div[class^="osumania"]'
+  );
+  for (const codeElement of codeElements) {
+    // Given the class is of form language-osumania-<Keys>
+    // Get the keys
+    const nKeys = parseInt(codeElement.className.match(/\d+/)[0]);
 
-  if (codeElement && codeElement.parentNode) {
-    codeElement.parentNode.insertBefore(canvas, codeElement.nextSibling);
-  }
-
-  // Extract and process the data from the <code> element
-  let data = codeElement.textContent;
-  codeElement.remove();
-  const notesRaw = data.match(/\d+(\|[\dA-Z]+)+/g);
-  let notes = notesRaw.map(part => {
-    let [time, col, meta] = part.split("|");
-    let color = "X", length = 0;
-    if (meta !== undefined) {
-      let colorMatch = meta.match(/[A-Z]/);
-      color = colorMatch?.[0] ?? "R";
-      let lengthMatch = meta.match(/\d+/);
-      length = parseInt(lengthMatch?.[0] ?? 0);
+    // Create a canvas element to draw
+    // We will replace the <code> element with this canvas
+    const canvas = document.createElement('canvas');
+    if (codeElement && codeElement.parentNode) {
+      codeElement.parentNode.insertBefore(canvas, codeElement.nextSibling);
     }
-    return [parseInt(time), parseInt(col), color, length];
-  });
-  const timeMin = notes.reduce(
-    (acc, [time]) => Math.min(acc, time), Infinity
-  );
-  const timeMax = notes.reduce(
-    (acc, [time]) => Math.max(acc, time), -Infinity
-  );
 
-  notes = notes.map(([time, col, color, length]) =>
-    [
-      (time - timeMin) / (timeMax - timeMin),
-      col, color,
-      length / (timeMax - timeMin)
-    ]
-  );
+    // Extract then delete the <code> element
+    let data = codeElement.textContent;
+    codeElement.remove();
 
-  canvas.width = width;
-  canvas.height = height;
+    // Extract the notes from the data
+    // This format should follow the editor format
+    const notesRaw = data.match(/\d+(\|[\dA-Z]+)+/g);
 
-  let nKeys = notes.reduce((acc, [_, col]) => Math.max(acc, col), 0) + 1
-    noteSpaceWidth = width / nKeys,
-    noteWidth = noteSpaceWidth * noteWidthScale,
-    noteHeight = canvas.height * noteHeightScale, lnWidth = noteWidth * 0.7,
-    ctx = canvas.getContext("2d");
+    // Preprocess the notes
+    let notes = notesRaw.map(part => {
+      // It should be in the format <time>|<column>|<meta>
+      // Meta includes the color and length of the note, which should include
+      // an uppercase letter and a number respectively (regardless of order)
+      // Meta is optional
+      let [time, col, meta] = part.split("|");
+      [time, col] = [time, col].map(x => parseInt(x));
 
-  ctx.fillStyle = "#333"
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#FFF';
-  ctx.strokeStyle = '#222';
+      let color, length;
+      if (meta !== undefined) {
+        let colorMatch = meta.match(/[A-Z]/);
+        color = colorMatch?.[0] ?? "R";
+        let lengthMatch = meta.match(/\d+/);
+        length = parseInt(lengthMatch?.[0] ?? 0);
+      }
 
-  let heightFenced = height / ((noteHeight + height) / height);
-  for (let [time, col, color, length] of notes) {
-    const colorMap = {
-      "R": '#ff4444',
-      "G": '#5eff5e',
-      "B": '#5fa9ff',
-    };
+      return [time, col, color, length];
+    });
 
-    ctx.fillStyle = colorMap[color] || '#DDD'; // Default color if not found
+    // Extract the lower and upper bounds of the time
+    const timeMin = notes.reduce(
+      (acc, [time]) => Math.min(acc, time), Infinity
+    );
+    const timeMax = notes.reduce(
+      (acc, [time]) => Math.max(acc, time), -Infinity
+    );
 
-    let x = col * noteSpaceWidth + (noteSpaceWidth - noteWidth) / 2;
-    let y = time * heightFenced;
-    let w = noteWidth;
-    let h = noteHeight;
-    ctx.fillRect(x, y, w, h);
+    // ... to normalize the time: (x - timeMin) / (timeMax - timeMin)
+    notes = notes.map(([time, col, color, length]) =>
+      [
+        (time - timeMin) / (timeMax - timeMin),
+        col, color,
+        length / (timeMax - timeMin)
+      ]
+    );
 
-    if (length > 0) {
-      let xln = x + (noteWidth - lnWidth) / 2;
-      let yln = y;
-      let wln = lnWidth;
-      let hln = length * heightFenced;
+    canvas.width = width;
+    canvas.height = height;
 
-      ctx.fillRect(xln, yln, wln, hln);
+    let noteSpaceWidth = width / nKeys,
+      noteWidth = noteSpaceWidth * noteWidthScale,
+      noteHeight = canvas.height * noteHeightScale, lnWidth = noteWidth * 0.7,
+      ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#333"
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#FFF';
+    ctx.strokeStyle = '#222';
+
+    // Height Fenced is the adjusted height of the canvas
+    // Which includes an additional height of the last note
+    let heightFenced = height / ((noteHeight + height) / height);
+
+    for (let [time, col, color, length] of notes) {
+      // Color mapping dictionary, default is gray
+      const colorMap = {
+        "R": '#ff4444',
+        "P": '#9f59ff',
+        "G": '#5eff5e',
+        "B": '#5fa9ff',
+      };
+
+      ctx.fillStyle = colorMap[color] || '#DDD'; // Default color if not found
+
+      // Handle Notes
+      let x = col * noteSpaceWidth + (noteSpaceWidth - noteWidth) / 2;
+      // Flip the y-axis to start from bottom
+      let y = (1 - time) * heightFenced;
+      let w = noteWidth;
+      let h = noteHeight;
+      ctx.fillRect(x, y, w, h);
+
+      // Handle LNs
+      if (length > 0) {
+        let xln = x + (noteWidth - lnWidth) / 2;
+        let yln = y;
+        let wln = lnWidth;
+        // Reverse the height to be to render tail upwards
+        let hln = -length * heightFenced;
+
+        ctx.fillRect(xln, yln, wln, hln);
+      }
     }
   }
 })();
--- _posts/2024-07-07-lapis-lazuli.md
+++ _posts/2024-07-07-lapis-lazuli.md
@@ -3,20 +3,242 @@ title: bohemianvoodoo - Lapis Lazuli [sculpteur d'émotion]
 tags: [ vsrg, osu!mania ]
 ---
 
-Test post (map not uploaded yet)
+We'll illustrate how we sculpted this piece
 
 <!--more-->
 
+# Background
+
+[Link to Map](https://osu.ppy.sh/beatmapsets/2210134#mania/4681404)
+
+_sculpteur d'émotion_ is one of my more unique entries. It's mapped for
+[osu!mania 4K French Tournament](https://osu.ppy.sh/community/forums/topics/1929981?n=1)
+(therefore the difficulty name).
+
+The approach, as mentioned in the description, is to have **singular focus**
+and **negative space**.
+
+## Singular Focus
+
+Many maps, especially harder maps, tend to achieve difficulty through more
+layers. This approach, while valid, tend to over-saturate the playfield. Take
+for example below:
+
 ```osumania-4
-00:28:264 (
-1000|0|R,
-1000|1|R,
-2000|3|G1000,
-3000|1|R0,
-4000|2|B0,
-5000|0|X0,
-) -
+451130|3|R,
+451130|0|R,
+451262|1|R,
+451393|3|R,
+451393|2|R,
+451525|0|R,
+451656|3|R,
+451656|1|R,
+451788|2|R,
+451920|3|R,
+451920|0|R,
+452051|2|R,
+452183|0|R,
+452183|1|R,
+
+451218|2|P,
+451306|0|P,
+451481|1|P,
+451569|2|P,
+451744|0|P,
+451832|1|P,
+452007|1|P,
+452095|3|P,
 ```
+
+The pattern layers a 1/4 stream (in red) and 1/6 fill (in purple).
+For this, it plays well as long as you don't accidentally throw in a 1/6 or 1/4
+jack.
+On the surface, this seem fine, but the intended perspective by the mapper
+often differs greatly from what the player sees.
+
+```osumania-4
+451130|3|R,
+451130|0|P,
+451218|2|R,
+451262|1|R,
+451306|0|R,
+451393|2|P,
+451393|3|P,
+451481|1|B,
+451525|0|B,
+451569|2|B,
+451656|3|B,
+451656|1|B,
+451744|0|R,
+451788|2|G,
+451832|1|R,
+451920|3|G,
+451920|0|R,
+452007|1|R,
+452051|2|R,
+452095|3|R,
+452183|0|B,
+452183|1|B,
+```
+
+The above is how the player would **break down** such a complex pattern, by
+glue-ing together neighbouring notes. In the end, the player likely didn't even
+notice they were playing 1/6 and 1/4 together.
+
+Commonly, a remedy to this is to glue 1/6s and 1/4s together, so it's easier to
+see.
+
+```osumania-4
+451130|3|P,
+451218|2|P,
+451306|3|P,
+451393|2|P,
+451481|3|P,
+451569|2|P,
+451656|0|P,
+451744|1|P,
+451832|0|P,
+451920|1|P,
+452007|0|P,
+452095|1|P,
+
+
+451130|0|R,
+451262|0|R,
+451393|1|R,
+451525|1|R,
+451656|3|R,
+451788|3|R,
+451920|2|R,
+452051|2|R,
+452183|3|R,
+452183|0|R,
+```
+
+However, the more you try to separate denser patterns, the more likely you'll
+get unintentionally difficult patterns. Here we introduced 1/4 jacks and 1/6
+trills. If it's truly impossible to separate them such that it's both
+**easy to play** and **easy to understand**. **Just remove notes**.
+
+### Players have limited Cognitive Capacity
+
+Sometimes, the simpler solution is to remove notes.
+
+> But there are sounds there!
+
+```mermaid
+flowchart TD
+  Z((Start Here)) --> A[Are those sounds important?]
+  A ==>|No| F((Don't map them))
+  A -->|Yes| B[Are there more than 1?]
+  B -->|Yes| C[Can you fit them without making a mess?]
+  C -->|Yes| Y((Map them))
+  B -->|No| Y
+  C -->|No| D[Can you pick the most important one?]
+  D -->|Yes| Y
+  D --->|No| E[Can you at least make them play well?]
+  E -->|Yes| Y
+  E ==>|No| F
+```
+
+I roughly follow the above train of thought when considering what to map, what
+to ignore, and such. Remember that people, while complex, likes simple.
+
+<table>
+<tr>
+  <td>
+    <div class="osumania-4">
+      451130|0|P,
+      451130|3|P,
+      451218|1|P,
+      451306|2|P,
+      451393|0|P,
+      451481|2|P,
+      451569|1|P,
+      451656|3|P,
+      451744|1|P,
+      451832|2|P,
+      451920|0|P,
+      452007|2|P,
+      452095|1|P,
+      452183|3|P,
+      452183|0|P,
+    </div>
+  </td>
+  <td>
+    <div class="osumania-4">
+      451130|0|P
+      451130|3|P
+      451218|1|P
+      451306|3|P
+      451393|1|P
+      451481|2|P
+      451569|0|P
+      451656|2|P
+      451656|3|P
+      451744|0|P
+      451832|2|P
+      451920|1|P
+      452007|3|P
+      452095|1|P
+      452183|3|P
+      452183|0|P
+    </div>
+  </td>
+  <td>
+    <div class="osumania-4">
+      451130|1|P,
+      451218|0|P,
+      451481|3|P,
+      451569|2|P,
+      451656|1|P,
+      451744|2|P,
+      451832|3|P,
+      452095|0|P,
+      452183|1|P,
+      451130|3|R,
+      451262|2|R,
+      451393|1|R,
+      451525|0|R,
+      451788|0|R,
+      451920|1|R,
+      452051|2|R,
+      452183|3|R,
+    </div>
+  </td>
+  <td>
+    <div class="osumania-4">
+      451130|3|P,
+      451218|2|P,
+      451306|1|P,
+      451393|0|P,
+      451481|1|P,
+      451569|2|P,
+      451656|3|P,
+      451744|2|P,
+      451832|1|P,
+      451920|0|P,
+      452007|1|P,
+      452095|2|P,
+      452183|3|P,
+      451130|0|R,
+      451393|3|R,
+      451656|0|R,
+      451920|3|R,
+      452183|0|R,
+    </div>
+  </td>
+</tr>
+</table>
+
+<style>
+canvas{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
 
 
 <script>{%- include scripts/lib/pattern-render.js -%}</script>
